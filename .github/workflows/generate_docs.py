8ebf60363073498577dfcc426c2cd029d54622fe
Robson P Oliveira <rpopuc@gmail.com>
2023-02-19 12:48:46 -0300

    feat: add the README file

--- .github/workflows/generate_docs.py
+++ .github/workflows/generate_docs.py
@@ -2,7 +2,6 @@ import os
 import git
 import sys
 import openai
-import difflib
 
 # Authenticate with OpenAI API
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -30,19 +29,14 @@ for line in diff_lines.splitlines(True):
     else:
         modified_lines.append(line)
 
-# Use the difflib library to find the longest common subsequence between the added and deleted lines
-matcher = difflib.SequenceMatcher(None, added_lines, deleted_lines)
-lcs = matcher.find_longest_match(0, len(added_lines), 0, len(deleted_lines))
-
 # Extract the added, deleted, and modified code blocks
-# print(added_lines)
 added_code = "".join(added_lines)
 deleted_code = "".join(deleted_lines)
 modified_code = "".join(modified_lines)
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt=f"Based only on this diffs:\nAdded code:\n{added_code}\nDeleted code:\n{deleted_code}\nModified code:\n{modified_code}.\n\nExplain the main changes, one per line, made on the code in terms of added, changed or removed funcionality.",
+  prompt=f"Based only on this diffs:\nAdded code:\n{added_code}\n\nDeleted code:\n{deleted_code}\n\nModified code:\n{modified_code}.\n\nExplain the main changes made to the code in terms of added, changed, or removed features, with one change per line.",
   temperature=0.7,
   max_tokens=400,
   top_p=1.0,
@@ -50,10 +44,5 @@ response = openai.Completion.create(
   presence_penalty=0.0
 )
 
-# # Extract the summary from the result
-summary = response.choices[0].text.strip()
-# summary = """1. Added code for the pull_request trigger type to generate comments.
-# 2. Added code to the add-comment step to create a comment on the pull request.
-# 3. Removed code for the workflow_dispatch trigger type."""
-
-print(summary)
+# Extract the summary from the result
+print(response.choices[0].text.strip())
---
+++ README.md
@@ -0,0 +1,25 @@
+# Lab: Changelog Generator
+
+A POC for a pipeline that generates a changelog for a given PR - using code diffs and OpenAI's API - and adds it to the PR as a comment.
+
+## Configuration
+
+The secret `OPENAI_API_KEY` must be defined in Github in order for the application to authenticate with OpenAI's API.
+
+## How it works?
+
+A python script [generate_docs.py](https://github.com/rpopuc/lab-pr-changelog-generator/blob/0acff565a14502db0a46697cf5a328219f429801/.github/workflows/generate_docs.py) gets the diffs from the current PR and the main branch, classifies these diffs in 'added', 'modified' and 'deleted' blocks, and then requests a description from OpenAI's API based on these blocks:
+
+```
+Based only on this diffs:
+Added code:
+{added_code}
+
+Deleted code:
+{deleted_code}
+
+Modified code:
+{modified_code}.
+
+Explain the main changes made to the code in terms of added, changed, or removed features, with one change per line.
+```
